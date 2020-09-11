79e70cc989f1fb303a70bceb9e628e4d75c24dd4
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-11 09:58:59 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Security;
+
+namespace chividientich
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap width:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap height:");
+            double height = Double.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+
+            Console.WriteLine("Tinh chu vi dien tich HCN co: \n" + rectangle.Display());
+            Console.WriteLine("Chu vi hinh chu nhat: " + rectangle.Chuvi());
+            Console.WriteLine("Dien tich hinh chu nhat la: " + rectangle.Dientich());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chividientich
+{
+    class Rectangle
+    {
+        double width, height;
+
+        public Rectangle()
+        {
+        }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public double Dientich()
+        {
+            return this.width * this.height;
+        }
+
+        public double Chuvi()
+        {
+            return (this.width + this.height) * 2;
+        }
+
+        public string Display()
+        {
+            return   "width=" + width + ", height=" + height;
+        }
+    }
+}
+
